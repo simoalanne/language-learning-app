9620d9aead11990a302547482e11ffce8ad50865
simoalanne <simo.alanne@tuni.fi>
2024-12-11 23:53:01 +0200

    Setup express server

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("express server");
+});
+
+const startServer = () => {
+  const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+  setupShutdown(server);
+};
+
+const setupShutdown = (server) => {
+  const shutdown = () => {
+    console.info("Shutting down server");
+    server.close((err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.info("Server shutdown");
+      process.exit(0);
+    });
+  };
+
+  process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
+};
+
+startServer();
