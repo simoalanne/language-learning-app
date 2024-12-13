382a8058c3bee16889e48b707c9caaa71330bfe7
simoalanne <simo.alanne@tuni.fi>
2024-12-13 02:41:56 +0200

    setup react project and enable cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { initDb, closeDb } from "./db.js";
 import wordGroupsRouter from "./routes/wordGroups.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/word-groups", wordGroupsRouter);
 
