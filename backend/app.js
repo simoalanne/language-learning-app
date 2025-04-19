0d6ecffc1c8ee2254c67338cb77e0f0af8f3e956
Simo <simo.alanne@tuni.fi>
2025-04-19 20:44:16 +0300

    Fix frontend navigation not working correctly in prod

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,16 @@ app.use(express.json());
 app.use(handleInvalidJsonError);
 app.use(enforceJsonContentType);
 app.use(express.static(publicPath));
-
 app.use("/api/word-groups", wordGroupsRouter);
 app.use("/api/ai/", aiRouter);
 app.use("/api/auth", authRouter);
+
+// needed for react router to work properly in production
+// when the user refreshes the page or tries to access a route directly
+// the server needs to send the index.html file so that react router can take over
+// and render the correct component
+app.get("*", (_, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 export default app;
